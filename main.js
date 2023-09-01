f44d778ddf5a47fff0978dec19cc0cb0eb32b260
SHYIRAMBERE Emmanuel <110242932+EmmanuelSHYIRAMBERE@users.noreply.github.com>
2023-09-01 11:59:28 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Freelancer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+    };
+
+    const serviceID = "service_lpu5bn4";
+    const templateID = "template_xnbv98k";
+
+    emailjs
+        .send(serviceID, templateID, params)
+        .then(res => {
+                document.getElementById("name").value = "";
+                document.getElementById("email").value = "";
+                document.getElementById("subject").value = "";
+                document.getElementById("message").value = "";
+                console.log(res);
+                alert("your message sent successfully");
+            })
+
+            .catch((err) => console.log(err));
+
+}
+
+
+
+    
