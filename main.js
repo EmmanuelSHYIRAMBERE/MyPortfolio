e732f76de004397d112c517fbbabf156a59f6b3d
EmmanuelSHYIRAMBERE <emashyirambere1@gmail.com>
2023-10-22 13:25:17 +0200

    committed

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 var typed = new Typed(".text", {
-    strings: ["Frontend Developer", "Freelancer", "Web Developer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
+  strings: ["Web Designer", "Backend Developer", "Freelancer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
 });
 
+const btn = document.getElementById("button");
 
-const btn = document.getElementById('button');
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
 
-document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+  btn.value = "Sending...";
 
-   btn.value = 'Sending...';
+  const serviceID = "service_feidnor";
+  const templateID = "template_mcl8zir";
 
-   const serviceID = 'service_feidnor';
-   const templateID = 'template_mcl8zir';
-
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent! Thank you for feedback.');
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = "Send Email";
+      alert("Sent! Thank you for feedback.");
       this.reset();
-    }, (err) => {
-      btn.value = 'Send Email';
+    },
+    (err) => {
+      btn.value = "Send Email";
       alert(JSON.stringify(err));
-    });
+    }
+  );
 });
